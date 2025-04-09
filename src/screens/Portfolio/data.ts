87b77a9f7301f6ec98b8e35abacd8c59c0dcb74e
Carlos Alí García <mrgarcia7@gmail.com>
2025-04-09 16:19:08 -0500

    [F-17]: carousel with Swiper dependency in Portfolio (#18)
    * F-17: add swiper dependency
    
    * F-17: use Swiper to display project gallery images in a carousel
    
    * F-17: add more custom content for Miami Yachting project

--- src/screens/Portfolio/data.ts
+++ src/screens/Portfolio/data.ts
@@ -12,7 +12,8 @@ export const projects = [
     ],
     desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
     type: "Website",
-    languages: "PHP, HTML, CSS, JS",
+    languages:
+      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
     country: "USA",
     liveURL: "www.demo.com",
   },
@@ -31,60 +32,76 @@ export const projects = [
     country: "USA",
     liveURL: "www.example.com",
   },
-  // {
-  //   title: "Website Design for Marketing Agency Startup",
-  //   subTitle: "Mobile App, App Design",
-  //   img: "assets/img/project-3.jpg",
-  //   desc1:
-  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  //   desc2:
-  //     "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  //   type: "Website",
-  //   languages: "PHP, HTML, CSS, JS",
-  //   platform: "Wordpress",
-  //   country: "USA",
-  //   liveURL: "www.xyz.com",
-  // },
-  // {
-  //   title: "Mobile app design",
-  //   subTitle: "Mobile App, App Design",
-  //   img: "assets/img/project-4.jpg",
-  //   desc1:
-  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  //   desc2:
-  //     "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  //   type: "Website",
-  //   languages: "PHP, HTML, CSS, JS",
-  //   platform: "Wordpress",
-  //   country: "USA",
-  //   liveURL: "www.mysite.com",
-  // },
-  // {
-  //   title: "Website Design for Marketing Agency Startup",
-  //   subTitle: "Mobile App, App Design",
-  //   img: "assets/img/project-5.jpg",
-  //   desc1:
-  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  //   desc2:
-  //     "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  //   type: "Website",
-  //   languages: "PHP, HTML, CSS, JS",
-  //   platform: "Wordpress",
-  //   country: "USA",
-  //   liveURL: "www.yoursite.com",
-  // },
-  // {
-  //   title: "Mobile app design",
-  //   subTitle: "Mobile App, App Design",
-  //   img: "assets/img/project-6.jpg",
-  //   desc1:
-  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  //   desc2:
-  //     "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  //   type: "Website",
-  //   languages: "PHP, HTML, CSS, JS",
-  //   platform: "Wordpress",
-  //   country: "USA",
-  //   liveURL: "www.realsite.com",
-  // },
+  {
+    title: "Miami Yachting – Sales Platform",
+    subTitle:
+      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
+    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+    galleryImages: [
+      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
+    ],
+    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
+    type: "Website",
+    languages:
+      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
+    country: "USA",
+    liveURL: "www.demo.com",
+  },
+  {
+    title: "Miami Yachting – Sales Platform",
+    subTitle:
+      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
+    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+    galleryImages: [
+      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
+    ],
+    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
+    type: "Website",
+    languages:
+      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
+    country: "USA",
+    liveURL: "www.demo.com",
+  },
+  {
+    title: "Miami Yachting – Sales Platform",
+    subTitle:
+      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
+    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+    galleryImages: [
+      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
+    ],
+    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
+    type: "Website",
+    languages:
+      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
+    country: "USA",
+    liveURL: "www.demo.com",
+  },
+  {
+    title: "Miami Yachting – Sales Platform",
+    subTitle:
+      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
+    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+    galleryImages: [
+      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
+      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
+    ],
+    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
+    type: "Website",
+    languages:
+      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
+    country: "USA",
+    liveURL: "www.demo.com",
+  },
 ];
