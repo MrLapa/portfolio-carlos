34f72f458dbb9dbc40491b35e2a45a0a4fb3bdf9
Carlos Garcia <mrgarcia7@gmail.com>
2025-04-10 10:31:13 -0500

    F-19: add content for Nerdwallet project

--- src/screens/Portfolio/data.ts
+++ src/screens/Portfolio/data.ts
@@ -1,5 +1,22 @@
 export const projects = [
   {
+    title: "Nerdwallet – Financial Services",
+    subTitle:
+      "A comprehensive financial services platform offering personalized advice and tools.",
+    thumbnail: "/assets/img/nerdwallet/nerdwallet-1.png",
+    galleryImages: [
+      "/assets/img/nerdwallet/nerdwallet-1.png",
+      "/assets/img/nerdwallet/nerdwallet-2.png",
+      "/assets/img/nerdwallet/nerdwallet-3.png",
+    ],
+    desc: "A comprehensive financial services platform offering personalized advice and tools to help users make informed decisions about their finances. The platform features a user-friendly interface, advanced filtering options, and a wealth of resources to assist users in navigating the complex world of personal finance.",
+    type: "Website",
+    languages:
+      "TypeScript, Next.js, Apollo Client, GraphQL, MUI, Zod, Jest, React Testing Library, Storybook",
+    country: "USA",
+    liveURL: "https://www.nerdwallet.com/",
+  },
+  {
     title: "Miami Yachting – Services & Experiences",
     subTitle:
       "Full-service marine platform offering yacht management, charters, and personalized boating experiences.",
@@ -33,24 +50,6 @@ export const projects = [
       "React Router, Redux, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
     country: "USA",
     liveURL: "https://sales.miamiyachting.net/",
-  },
-  {
-    title: "Miami Yachting – Sales Platform",
-    subTitle:
-      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
-    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
-    galleryImages: [
-      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
-    ],
-    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
-    type: "Website",
-    languages:
-      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
-    country: "USA",
-    liveURL: "www.demo.com",
   },
   {
     title: "Miami Yachting – Sales Platform",
