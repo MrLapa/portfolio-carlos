781475f0c3a12afd447efdcf5482b8eb3ff3f209
Carlos Garcia <mrgarcia7@gmail.com>
2025-04-10 21:49:33 -0500

    F-20: add content for Evidation project

--- src/screens/Portfolio/data.ts
+++ src/screens/Portfolio/data.ts
@@ -52,57 +52,21 @@ export const projects = [
     liveURL: "https://sales.miamiyachting.net/",
   },
   {
-    title: "Miami Yachting – Sales Platform",
+    title: "Evidation App – Health & Wellness",
     subTitle:
-      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
-    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
+      "A health and wellness app that rewards users for healthy activities.",
+    thumbnail: "/assets/img/evidation/evidation-1.png",
     galleryImages: [
-      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
+      "/assets/img/evidation/evidation-1.png",
+      "/assets/img/evidation/evidation-2.png",
+      "/assets/img/evidation/evidation-3.png",
+      "/assets/img/evidation/evidation-4.png",
     ],
-    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
-    type: "Website",
+    desc: "A health and wellness app that rewards users for healthy activities. The platform features a user-friendly interface, personalized recommendations, and a variety of tools to help users track their progress and achieve their health goals. The app is designed to be engaging and motivating, encouraging users to stay active and make healthier choices.",
+    type: "Cross-Platform App",
     languages:
-      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
+      "TypeScript, TanStack Query, Axios, React Native, Zod, Zustand, Yup, React Hook Form",
     country: "USA",
-    liveURL: "www.demo.com",
-  },
-  {
-    title: "Miami Yachting – Sales Platform",
-    subTitle:
-      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
-    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
-    galleryImages: [
-      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
-    ],
-    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
-    type: "Website",
-    languages:
-      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
-    country: "USA",
-    liveURL: "www.demo.com",
-  },
-  {
-    title: "Miami Yachting – Sales Platform",
-    subTitle:
-      "A global listing platform to discover and sell yachts, motor yachts, cruisers, and more.",
-    thumbnail: "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
-    galleryImages: [
-      "/assets/img/miami-yachting-sales/miami-yachting-services-1.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-2.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-3.png",
-      "/assets/img/miami-yachting-sales/miami-yachting-services-4.png",
-    ],
-    desc: "A global marketplace built to help users browse, filter, and list a wide range of vessels including yachts, motor yachts, express cruisers, center consoles, and sailboats. Designed for performance and usability, this platform allows individuals and brokers to showcase vessels with detailed specs and high-quality images, making it easy for buyers around the world to explore available options. It features a search-friendly layout, intuitive filtering, and responsive design, all built with modern web technologies to ensure a smooth experience across devices.",
-    type: "Website",
-    languages:
-      "TypeScript, Gatsby, Axios, Styled components, MUI, Sass, Jest, React Testing Library",
-    country: "USA",
-    liveURL: "www.demo.com",
+    liveURL: "https://my.evidation.com/login",
   },
 ];
