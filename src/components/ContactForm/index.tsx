74066c13347b970635b09f28e87dc6cbb3643674
Carlos Alí García <mrgarcia7@gmail.com>
2025-04-12 10:53:44 -0500

    [F-21]: Contact form (#22)
    * F-21: install react-hooh-form dependency
    
    * F-21: create and implement ContactForm component
    
    * F-21: install Yup dependency
    
    * F-21: install @hookform/resolvers dependency
    
    * F-21: implement validation schema for ContactForm fields
    
    * F-21: create styles for disabled elements
    
    * F-21: create custom Input component
    
    * F-21: implement custom Input component in ContactForm and add styles for disabled button
    
    * F-21: redesign Contact section
    
    * F-21: add new line at EOF
    
    * F-21: add new line at EOF

---
+++ src/components/ContactForm/index.tsx
@@ -0,0 +1,119 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FieldValues, useForm } from 'react-hook-form';
+import * as yup from "yup"
+import Input from '../Input';
+
+const validationSchema = yup
+  .object({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .email("Email is not valid")
+      .required("Email is required"),
+    subject: yup.string().required("Subject is required"),
+    msg: yup.string().nullable(),
+  })
+  .required()
+
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+    reset,
+  } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
+
+  const onSubmit = async (data: FieldValues) => {
+    console.log("Form Submitted:", data);
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+    reset();
+  };
+
+  return (
+    <div className="contact-form">
+      <div className="contact-head">
+        <h4>Got Ideas? I&apos;ve got the skills. Let&apos;s team up!</h4>
+        <p>Share your vision, and I&apos;ll help bring it to life.</p>
+      </div>
+      <form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
+        {isSubmitSuccessful && (
+          <div
+            className="returnmessage"
+            data-success="Your message has been received, we will contact you soon."
+            style={{ display: "block" }}
+          >
+            <span className="contact_success">
+              Your message has been received, we will contact you soon.
+            </span>
+          </div>
+        )}
+        <div className="row gx-3 gy-4">
+          <div className="col-md-6">
+            <div className="form-group">
+              <Input
+                register={register}
+                id="name"
+                label="First name"
+                placeholder="Name *"
+                error={errors.name}
+              />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <Input
+                register={register}
+                id="email"
+                label="Your email"
+                placeholder="E-mail *"
+                error={errors.email}
+              />
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="form-group">
+              <Input
+                register={register}
+                id="subject"
+                label="Subject"
+                placeholder="Subject *"
+                error={errors.subject}
+              />
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="form-group">
+              <label htmlFor='message' className="form-label">Your message</label>
+              <textarea
+                {...register("msg")}
+                name="msg"
+                id="message"
+                placeholder="Message"
+                className={`form-control ${errors.msg ? "is-invalid" : ""}`}
+              />
+              {errors.msg && (
+                <span className="text-danger">{errors.msg.message}</span>
+              )}
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="send">
+              <button
+                id="send_message"
+                type="submit"
+                disabled={isSubmitting}
+                className={`px-btn w-100 ${isSubmitting ? "disabled" : ""}`}
+              >
+                {isSubmitting ? "Sending..." : "Send Message"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
--- src/screens/ContactUs/index.tsx
+++ src/screens/ContactUs/index.tsx
@@ -1,37 +1,11 @@
+import ContactForm from "@/src/components/ContactForm"
+
 const ContactUs = () => {
   return (
     <div className="container">
-      <div className="row gy-5">
-        <div className="col-lg-5 d-flex">
-          <div className="d-flex flex-column w-100">
-            <div className="contact-info">
-              <h3>Let’s Discuss Your Project</h3>
-              <ul>
-                <li>
-                  <div className="icon">
-                    <i className="bi-chat-left-dots-fill" />
-                  </div>
-                  <div className="text">
-                    <label>Chat to us</label>
-                    <p>
-                      Our friendly team is there to help.{" "}
-                      <span>
-                        <a
-                          className="text-reset"
-                          href="mailto:info@domainname.com"
-                        >
-                          info@domainname.com
-                        </a>
-                      </span>
-                    </p>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-7 ps-xl-5">
-          {/* <ContactForm /> */}
+      <div className="row">
+        <div className="col-12">
+          <ContactForm />
         </div>
       </div>
     </div>
