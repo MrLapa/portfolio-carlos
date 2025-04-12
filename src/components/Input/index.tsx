1ec24a3f99619ce50619a65dee24623a80bd7f12
Carlos Garcia <mrgarcia7@gmail.com>
2025-04-11 22:07:15 -0500

    F-21: create custom Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FieldError, FieldValues, Path, UseFormRegister } from "react-hook-form";
+
+interface InputProps<T extends FieldValues> {
+  id: Path<T>;
+  label: string;
+  register: UseFormRegister<T>;
+  type?: string;
+  placeholder?: string;
+  error?: FieldError;
+}
+
+const Input = <T extends FieldValues>({
+  id,
+  label,
+  register,
+  type = "text",
+  placeholder,
+  error,
+}: InputProps<T>) => {
+  return (
+    <>
+      <label className="form-label" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        {...register(id)}
+        id={String(id)}
+        type={type}
+        placeholder={placeholder}
+        className={`form-control ${error ? "is-invalid" : ""}`}
+      />
+      {error && <span className="text-danger">{error.message}</span>}
+    </>
+  );
+};
+
+export default Input;
