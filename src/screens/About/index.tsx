afd2075917a07c462ddeba01661054cdf3d83166
Carlos Garcia <mrgarcia7@gmail.com>
2025-04-15 15:57:14 -0500

    F-23: remove number of completed projects section and refactor experiences section

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -13,6 +13,43 @@ const skills = [
   { name: "Version Control (Git, GitHub)", value: 90 },
 ]
 
+const experiences = [
+  {
+    company: "Wizeline SAS",
+    position: "Software Engineer",
+    startDate: "2023",
+    endDate: "Present",
+    description: "Building performant UIs with MUI and Next.js. Writing unit tests with Jest and React Testing Library. Using TanStack Query and GraphQL for efficient data handling.",
+    location: "Colombia"
+  },
+  {
+    company: "Miami Yachting",
+    position: "Frontend Developer",
+    startDate: "2024",
+    endDate: "2025",
+    description: "Developing a yacht marketplace with Gatsby, Axios, and Styled Components. Implemented dynamic filters, responsive UI, and fast-loading pages for a seamless search experience.",
+    location: "Miami, FL"
+  },
+  {
+    company: "BoatyardX SAS",
+    position: "Frontend Developer",
+    startDate: "2021",
+    endDate: "2022",
+    description: "Building responsive financial apps with Next.js and TypeScript. Created reusable components with Storybook and integrated GraphQL APIs. Ensured quality with Jest and Cypress tests.",
+    location: "Colombia"
+  },
+  {
+    company: "Falabella",
+    position: "Software Developer",
+    startDate: "2019",
+    endDate: "2020",
+    description: "Developing ASP.NET MVC and React apps. Built Web APIs and handled Oracle DBs for enterprise-grade solutions.",
+    location: "Colombia"
+  }
+]
+
+
+
 const About = () => {
   return (
     <div className="container">
@@ -25,20 +62,6 @@ const About = () => {
             <div className="about-me-text pb-5">
               <p>I&apos;m a Software Engineer with 8 years of experience building high-quality web applications, specializing in React, TypeScript, and front-end technologies. I thrive in agile teams, strive to write clean, maintainable code, and focus on creating smooth, user-friendly experiences.
                 Driven by curiosity and a strong commitment to best practices, I bring a proactive approach to problem-solving and continuous improvement.</p>
-              <div className="row pt-2">
-                <div className="col-auto">
-                  <div className="a-count">
-                    <span className="count">5k</span>
-                    <div className="a-count-text">Projects <br />Completed.</div>
-                  </div>
-                </div>
-                <div className="col-auto">
-                  <div className="a-count">
-                    <span className="count">3k</span>
-                    <div className="a-count-text">Satisfied <br />Clients.</div>
-                  </div>
-                </div>
-              </div>
             </div>
             <hr />
             <div className="title-01 mt-5">
@@ -46,40 +69,15 @@ const About = () => {
             </div>
             <div className="resume-box">
               <ul>
-                <li>
-                  <div className="icon">
-                    <i className="fas fa-briefcase" />
-                  </div>
-                  <span className="time">2023 - Present</span>
-                  <h5>Software Engineer – Wizeline SAS (Colombia)</h5>
-                  <p>Building performant UIs with MUI and Next.js. Writing unit tests with Jest and React Testing Library. Using TanStack Query and GraphQL for efficient data handling.</p>
-                </li>
-                <li>
-                  <div className="icon">
-                    <i className="fas fa-briefcase" />
-                  </div>
-                  <span className="time">2024 - 2025</span>
-                  <h5>Frontend Developer – Miami Yachting (Freelance)</h5>
-                  <p>
-                    Developed a yacht marketplace with Gatsby, Axios, and Styled Components. Implemented dynamic filters, responsive UI, and fast-loading pages for a seamless search experience.
-                  </p>
-                </li>
-                <li>
-                  <div className="icon">
-                    <i className="fas fa-briefcase" />
-                  </div>
-                  <span className="time">2021 - 2022</span>
-                  <h5>Frontend Developer – BoatyardX SAS (Colombia)</h5>
-                  <p>Building responsive financial apps with Next.js and TypeScript. Created reusable components with Storybook and integrated GraphQL APIs. Ensured quality with Jest and Cypress tests.</p>
-                </li>
-                <li>
-                  <div className="icon">
-                    <i className="fas fa-briefcase" />
-                  </div>
-                  <span className="time">2019 - 2020</span>
-                  <h5>Software Developer – Falabella (Colombia)</h5>
-                  <p>Developing ASP.NET MVC and React apps. Built Web APIs and handled Oracle DBs for enterprise-grade solutions.</p>
-                </li>
+                {experiences.map((experience, index) => (
+                  <li key={`experience-${index}`}>
+                    <div className="icon">
+                      <i className="bi bi-briefcase-fill" />
+                    </div>
+                    <span className="time">{experience.startDate} - {experience.endDate}</span>
+                    <h5>{experience.position} – {experience.company} ({experience.location})</h5>
+                    <p>{experience.description}</p>
+                  </li>))}
               </ul>
             </div>
           </div>
