bcf6afeb59a5e42294b9644564609c0e1ec744d5
Carlos Garcia <mrgarcia7@gmail.com>
2025-04-15 15:09:22 -0500

    F-23: create useIsMobile custom hook to validate if the app is in a mobile breakpoint

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 991px)");
+
+    const handleChange = (e: MediaQueryListEvent) => setIsMobile(e.matches);
+
+    setIsMobile(mediaQuery.matches);
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
